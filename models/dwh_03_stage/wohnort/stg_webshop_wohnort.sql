ae15ce9763fba790fe91fdb6104920bd84d98cab
AndreasHaasErlangen <52035670+AndreasHaasErlangen@users.noreply.github.com>
2024-07-12 16:17:43 +0200

    new objects

--- models/dwh_03_stage/wohnort/stg_webshop_wohnort.sql
+++ models/dwh_03_stage/wohnort/stg_webshop_wohnort.sql
@@ -2,43 +2,36 @@
 
 {%- set yaml_metadata -%}
 source_model: 
-  'load_webshop_kunde'
+  'load_webshop_wohnort'
+hashed_columns:
+  hk_customer_h:
+    - kundeid
+  hd_customer_ws_la_ms:
+    is_hashdiff: true
+    columns:
+      - adresszusatz
+      - bis
+      - hausnummer
+      - land
+      - ort
+      - plz
+      - strasse
+
+
 derived_columns:
-    associationpartner_bk:
-      - vereinspartnerid
     customer_bk:
       - kundeid
 
+
     cdts:
       - {{var("local_timestamp")}}
+
     edts:      
       - edts_in
-
 
 rsrc: 'rsrc' 
 ldts: 'ldts'
 include_source_columns: true
-hashed_columns:
-  hk_associationpartner_h:
-    - vereinspartnerid
-  hk_customer_h:
-    - kundeid
-  hk_customer_associationpartner_l:
-    - customer_bk
-    - associationpartner_bk
-  hd_customer_ws_s:
-    is_hashdiff: true
-    columns:
-      - email
-      - geburtsdatum
-      - geschlecht
-      - gueltigbis
-      - kkfirma
-      - kreditkarte
-      - mobil
-      - name
-      - telefon
-      - vorname
 
 {%- endset -%}
 
@@ -48,4 +41,4 @@ hashed_columns:
                   source_model=metadata_dict['source_model'],
                   hashed_columns=metadata_dict['hashed_columns'],
                   derived_columns=metadata_dict['derived_columns']) }}
-where is_check_ok or rsrc ='SYSTEM'                  
+where is_check_ok or rsrc ='SYSTEM'                      
