3cbc83be8d4e9f0c372450702324d66c58aa27fb
AndreasHaasErlangen <52035670+AndreasHaasErlangen@users.noreply.github.com>
2024-07-21 11:26:50 +0200

    ref

--- models/dwh_04_rv/reference/category_deliveryadherence/category_deliveryadherence_r.sql
+++ models/dwh_04_rv/reference/category_deliveryadherence/category_deliveryadherence_r.sql
@@ -1,12 +1,13 @@
-{# template hub Version:0.1.0 #}
-{# automatically generated based on dataspot#}
-
 {{ config(materialized='incremental') }}
 
 {%- set source_model = 'stg_misc_kategorie_termintreue'           -%}
 {%- set src_pk = 'category_deliveryadherence_nk'                          -%}
+{%- set src_ldts = 'ldts'                          -%}
+{%- set src_source = 'rsrc'                          -%}
 
 {{ automate_dv.ref_table(src_pk=src_pk, 
-                         source_model=source_model) }}
+                         source_model=source_model,
+                         src_ldts=src_ldts,
+                         src_source=src_source) }}
 
 
