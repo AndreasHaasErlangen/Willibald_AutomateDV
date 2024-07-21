5a5d70d524a4d9bb244cb7f47f433462b0d6b6da
AndreasHaasErlangen <52035670+AndreasHaasErlangen@users.noreply.github.com>
2024-07-21 11:43:06 +0200

    working_sample

--- models/dwh_04_rv/reference/category_deliveryadherence/category_deliveryadherence_misc_rs.sql
+++ models/dwh_04_rv/reference/category_deliveryadherence/category_deliveryadherence_misc_rs.sql
@@ -1,13 +1,21 @@
-{# template hub Version:0.1.0 #}
-{# automatically generated based on dataspot#}
-
 {{ config(materialized='incremental') }}
 
-{%- set source_model = 'stg_misc_kategorie_termintreue'           -%}
-{%- set src_pk = 'category_deliveryadherence_nk'                          -%}
-{%- set src_extra_columns = ['count_days_from', 'count_days_to', 'name'] -%}
+{%- set yaml_metadata -%}
+source_model: 
+    - stg_misc_kategorie_termintreue
+src_pk: 'category_deliveryadherence_nk'
+src_extra_columns:
+    - count_days_from
+    - count_days_to
+    - name
+src_ldts: ldts
+src_source: rsrc
+{%- endset -%}
 
-{{ automate_dv.ref_table(src_pk=src_pk, 
-                         src_extra_columns=src_extra_columns,
-                         source_model=source_model) }}
+{% set metadata_dict = fromyaml(yaml_metadata) %}
 
+{{ automate_dv.ref_table(src_pk=metadata_dict["src_pk"],
+                   src_ldts=metadata_dict["src_ldts"],
+                   src_source=metadata_dict["src_source"],
+                   source_model=metadata_dict["source_model"], 
+                   src_extra_columns=metadata_dict["src_extra_columns"]) }}
