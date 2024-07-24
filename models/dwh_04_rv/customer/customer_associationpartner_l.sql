7140f4e44e84ce5122174c752c3957443113af44
AndreasHaasErlangen <52035670+AndreasHaasErlangen@users.noreply.github.com>
2024-07-24 11:39:21 +0200

    link

---
+++ models/dwh_04_rv/customer/customer_associationpartner_l.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='incremental') }}
+
+{%- set yaml_metadata -%}
+source_model: 
+  - stg_webshop_kunde
+src_pk: hk_customer_associationpartner_l
+src_fk: 
+    - 'hk_customer_h'
+    - 'hk_associationpartner_h'
+src_ldts: ldts
+src_source: rsrc
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.link(src_pk=metadata_dict["src_pk"],
+                    src_fk=metadata_dict["src_fk"], 
+                    src_ldts=metadata_dict["src_ldts"],
+                    src_source=metadata_dict["src_source"], 
+                    source_model=metadata_dict["source_model"]) }}
